496c4520968dc6deae8950606f7497d243a3cc63
tsvtln <tsvtln@abv.bg>
2024-02-05 12:26:56 +0200

    Update 03_animal_type.js

--- basics/conditional_statements_advanced/Lab/03_animal_type.js
+++ basics/conditional_statements_advanced/Lab/03_animal_type.js
@@ -1,1 +1,21 @@
+function animalType(input) {
+    let animal = String(input[0]);
+    let to_print = '';
+    switch (animal) {
+        case 'dog':
+            to_print = 'mammal';
+            break;
+        case 'crocodile':
+        case 'tortoise':
+        case 'snake':
+            to_print = 'reptile';
+            break;
+        default:
+            to_print = 'unknown';
+    }
+    console.log(to_print);
+}
 
+// animalType(["dog"]);
+// animalType(["snake"]);
+// animalType(["cat"]);
