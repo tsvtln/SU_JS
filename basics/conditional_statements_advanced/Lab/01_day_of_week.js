0821914e04a71674fb4bbc5e21c00e82fb9d3911
tsvtln <tsvtln@abv.bg>
2024-02-05 12:23:24 +0200

    01 02

--- basics/conditional_statements_advanced/Lab/01_day_of_week.js
+++ basics/conditional_statements_advanced/Lab/01_day_of_week.js
@@ -1,1 +1,39 @@
+function dayOfTheWeek(input) {
+    let number = Number(input[0]);
+    let to_print = '';
+    switch (number) {
+        case 1:
+            to_print = 'Monday';
+            break;
+        case 2:
+            to_print = 'Tuesday';
+            break;
+        case 3:
+            to_print = 'Wednesday';
+            break;
+        case 4:
+            to_print = 'Thursday';
+            break;
+        case 5:
+            to_print = 'Friday';
+            break;
+        case 6:
+            to_print = 'Saturday';
+            break;
+        case 7:
+            to_print = 'Sunday';
+            break;
+        default:
+            to_print = 'Error';
+    }
+    console.log(to_print);
+}
 
+// dayOfTheWeek(["1"]);
+// dayOfTheWeek(["2"]);
+// dayOfTheWeek(["3"]);
+// dayOfTheWeek(["4"]);
+// dayOfTheWeek(["5"]);
+// dayOfTheWeek(["6"]);
+// dayOfTheWeek(["7"]);
+// dayOfTheWeek(["-1"]);
--- basics/conditional_statements_advanced/Lab/02_weekend_or_working_day.js
+++ basics/conditional_statements_advanced/Lab/02_weekend_or_working_day.js
@@ -1,1 +1,24 @@
+function weekendOrWorkingDay(input) {
+    let current_day = String(input[0]);
+    let to_print = ''
+    switch (current_day) {
+        case 'Monday':
+        case 'Tuesday':
+        case 'Wednesday':
+        case 'Thursday':
+        case 'Friday':
+            to_print = 'Working day';
+            break;
+        case 'Saturday':
+        case 'Sunday':
+            to_print = 'Weekend';
+            break;
+        default:
+            to_print = 'Error';
+    }
+    console.log(to_print);
+}
 
+// weekendOrWorkingDay(["Monday"]);
+// weekendOrWorkingDay(["Sunday"]);
+// weekendOrWorkingDay(["April"]);
