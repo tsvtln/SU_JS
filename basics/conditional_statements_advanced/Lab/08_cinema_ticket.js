9b8490d613f2147a35cb572fa9b0956dc5b6798e
tsvtln <tsvtln@abv.bg>
2024-02-05 13:16:42 +0200

    09

--- basics/conditional_statements_advanced/Lab/08_cinema_ticket.js
+++ basics/conditional_statements_advanced/Lab/08_cinema_ticket.js
@@ -1,1 +1,18 @@
+function cinemaTicket(input) {
+    let day = String(input[0]);
+    let to_print = '';
+    let prices = {
+        Monday: 12,
+        Tuesday: 12,
+        Wednesday: 14,
+        Thursday: 14,
+        Friday: 12,
+        Saturday: 16,
+        Sunday: 16
+    }
+    console.log(prices[day]);
+}
 
+// cinemaTicket(["Monday"]);
+// cinemaTicket(["Friday"]);
+// cinemaTicket(["Sunday"]);
--- basics/conditional_statements_advanced/Lab/09_fruit_or_vegetable.js
+++ basics/conditional_statements_advanced/Lab/09_fruit_or_vegetable.js
@@ -1,1 +1,27 @@
+function fruitOrVegetable(input) {
+    let to_print = ''
+    switch (input[0]) {
+        case 'banana':
+        case 'apple':
+        case 'kiwi':
+        case 'cherry':
+        case 'lemon':
+        case 'grapes':
+            to_print = 'fruit';
+            break;
+        case 'tomato':
+        case 'cucumber':
+        case 'pepper':
+        case 'carrot':
+            to_print = 'vegetable';
+            break;
+        default:
+            to_print = 'unknown';
+    }
+    console.log(to_print);
+}
 
+// fruitOrVegetable(["banana"]);
+// fruitOrVegetable(["apple"]);
+// fruitOrVegetable(["tomato"]);
+// fruitOrVegetable(["water"]);
