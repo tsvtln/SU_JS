0a72f644ce45da241aef12436aa9e569b7686151
tsvtln <tsvtln@abv.bg>
2024-02-05 13:07:21 +0200

    resolutions

--- basics/conditional_statements_advanced/Lab/06_numbers_in_range.js
+++ basics/conditional_statements_advanced/Lab/06_numbers_in_range.js
@@ -1,1 +1,23 @@
+function numbersInRange(input) {
+    let number = Number(input[0]);
+    let to_print = '';
 
+    switch (true) {
+        case (number === 0):
+            to_print = 'No';
+            break;
+        case (number >= 1 && number <= 100):
+            to_print = 'Yes';
+            break;
+        case (number >= -100 && number <= -1):
+            to_print = 'Yes';
+            break;
+        default:
+            to_print = 'No';
+    }
+    console.log(to_print);
+}
+
+// numbersInRange(["-25"]);
+// numbersInRange(["0"]);
+// numbersInRange(["25"]);
--- basics/conditional_statements_advanced/Lab/07_working_hours.js
+++ basics/conditional_statements_advanced/Lab/07_working_hours.js
@@ -1,1 +1,32 @@
+function workingHours(input) {
+    let hour = Number(input[0]);
+    let day = String(input[1]);
+    let to_print = '';
 
+    let working_days = {
+        Monday: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        Tuesday: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        Wednesday: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        Thursday: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        Friday: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        Saturday: [10, 11, 12, 13, 14, 15, 16, 17, 18]
+    }
+
+    if (day in working_days) {
+        if (working_days[day].includes(hour)) {
+            to_print = 'open';
+        }
+        else {
+            to_print = 'closed';
+        }
+    }
+    else {
+        to_print = 'closed';
+    }
+
+    console.log(to_print);
+}
+
+// workingHours(["11", "Monday"]);
+// workingHours(["19", "Friday"]);
+// workingHours(["11", "Sunday"]);
