1ae237243fc666968b2f8ff7c6e686156130898b
tsvtln <tsvtln@abv.bg>
2024-02-05 12:33:27 +0200

    Update 04_personal_titles.js

--- basics/conditional_statements_advanced/Lab/04_personal_titles.js
+++ basics/conditional_statements_advanced/Lab/04_personal_titles.js
@@ -1,1 +1,30 @@
+function personalTitles(input) {
+    
+    let age = Number(input[0]);
+    let gender = String(input[1]);
+    let to_print = '';
 
+    if (gender === 'm') {
+        if (age < 16) {
+            to_print = 'Master';
+        }
+        else {
+            to_print = 'Mr.';
+        }
+    }
+
+    else if (gender === 'f') {
+        if (age < 16) {
+            to_print = 'Miss';
+        }
+        else {
+            to_print = 'Ms.'
+        }
+    }
+    console.log(to_print);
+}
+
+// personalTitles(["12", "f"]);
+// personalTitles(["17", "m"]);
+// personalTitles(["25", "f"]);
+// personalTitles(["13.5", "m"]);
