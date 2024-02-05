af0ef9d1f9699d60f15436fc8ca832b15d145b32
tsvtln <tsvtln@abv.bg>
2024-02-05 13:52:26 +0200

    fruit shop

--- basics/conditional_statements_advanced/Lab/10_invalid_number.js
+++ basics/conditional_statements_advanced/Lab/10_invalid_number.js
@@ -1,1 +1,18 @@
+function invalidNumber(input) {
+    let number = Number(input[0]);
+    let to_print = '';
 
+    if (!(number >= 100 && number <= 200) && number !== 0) {
+        to_print = 'invalid';
+    }
+    console.log(to_print);
+}
+
+// invalidNumber(["75"]);
+// invalidNumber(["150"]);
+// invalidNumber(["220"]);
+// invalidNumber(["199"]);
+// invalidNumber(["-1"]);
+// invalidNumber(["100"]);
+// invalidNumber(["200"]);
+// invalidNumber(["0"]);
--- basics/conditional_statements_advanced/Lab/11_fruit_shop.js
+++ basics/conditional_statements_advanced/Lab/11_fruit_shop.js
@@ -1,1 +1,58 @@
+function fruitShop(input) {
+    
+    let fruit = input[0];
+    let day = input[1];
+    let quantity = Number(input[2]);
+    let to_print = 0;
+    let prices = {
+        Workday: {
+            banana: 2.50,
+            apple: 1.20,
+            orange: 0.85,
+            grapefruit: 1.45,
+            kiwi: 2.70,
+            pineapple: 5.50,
+            grapes: 3.85
+        },
+        Weekend: {
+            banana: 2.70,
+            apple: 1.25,
+            orange: 0.90,
+            grapefruit: 1.60,
+            kiwi: 3.00,
+            pineapple: 5.60,
+            grapes: 4.20
+        }
+    }
+    if (fruit in prices['Workday']) {
+        switch (day) {
+            case 'Monday':
+            case 'Tuesday':
+            case 'Wednesday':
+            case 'Thursday':
+            case 'Friday':
+                to_print = prices['Workday'][fruit] * quantity;
+                break;
+            case 'Saturday':
+            case 'Sunday':
+                to_print = prices['Weekend'][fruit] * quantity;
+        }
+    }
+    else {
+        to_print = 'error';
+    }
 
+    if (typeof to_print === 'number'){
+    console.log(to_print.toFixed(2));
+    }
+    else {
+        console.log(to_print);
+    }
+
+}
+
+fruitShop(["apple", "Tuesday", "2"]);
+fruitShop(["orange", "Sunday", "3"]);
+fruitShop(["kiwi", "Monday", "2.5"]);
+fruitShop(["grapes", "Saturday", "0.5"]);
+fruitShop(["tomato", "Monday", "0.5"]);
