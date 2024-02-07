5dd0071af82536e9fa9a072e50c20400a872c521
tsvtln <tsvtln@abv.bg>
2024-02-07 13:14:28 +0200

    up

---
+++ basics/for_loop/Lab/03_numbers_1_n_step_3.js
@@ -0,0 +1,12 @@
+function step(input) {
+    number = Number(input[0]);
+    for (let i = 1; i  <= number; i += 3) {
+        console.log(i);
+    }
+}
+
+// step(["10"]);
+// console.log('\n')
+// step(["7"]);
+// console.log('\n')
+// step(["15"]);
