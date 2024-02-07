bb6a21d08413525acca6b9acf13413b72a5bbcf3
tsvtln <tsvtln@abv.bg>
2024-02-07 12:58:15 +0200

    Update 02_numbers_n_1.js

---
+++ basics/for_loop/Lab/02_numbers_n_1.js
@@ -0,0 +1,10 @@
+function reverse(input) {
+    let n = Number(input[0]);
+    for (let i = n; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+// reverse(["2"]);
+// reverse(["3"]);
+// reverse(["5"]);
