090bbbb9aae7b67ce5a8c26eb9087e8146db44c2
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-02-28 09:50:46 +0000

    07

---
+++ front_end_js/03_syntax_fund_exercises/07_chars_to_string.js
@@ -0,0 +1,13 @@
+function chars_to_string(ch1, ch2, ch3) {
+    console.log(ch1+ch2+ch3);
+}
+
+chars_to_string('a',
+'b',
+'c');
+chars_to_string('%',
+'2',
+'o');
+chars_to_string('1',
+'5',
+'p')
