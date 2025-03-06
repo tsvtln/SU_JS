30f6d9775389c5827fb94a069cb73721461f5123
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-03-06 21:06:57 +0200

    Update 09_loading_bar.js

--- front_end_js/08_functions_and_statements_exercise/09_loading_bar.js
+++ front_end_js/08_functions_and_statements_exercise/09_loading_bar.js
@@ -1,20 +1,31 @@
-function loadingBar(pct){
-    const loaded = pct / 10;
-    tp = '';
-    let bar = Array(loaded).fill('%');
-    let isLoaded = false;
-    if(bar.length===10){
-        isLoaded = true;
-    }
-    bar.push((Array(10 - loaded).fill('.')).join(''));
-    if(isLoaded){
-        tp = `100% Complete!\n[${bar.join('')}]`
-    }
-    else{
-        tp = `${loaded}0% [${bar.join('')}]\nStill loading...`
-    }
-    console.log(tp);
+// function loadingBar(pct){
+//     const loaded = pct / 10;
+//     tp = '';
+//     let bar = Array(loaded).fill('%');
+//     let isLoaded = false;
+//     if(bar.length===10){
+//         isLoaded = true;
+//     }
+//     bar.push((Array(10 - loaded).fill('.')).join(''));
+//     if(isLoaded){
+//         tp = `100% Complete!\n[${bar.join('')}]`
+//     }
+//     else{
+//         tp = `${loaded}0% [${bar.join('')}]\nStill loading...`
+//     }
+//     console.log(tp);
 
+// }
+
+function loadingBar(pct) {
+    const loaded = Math.floor(pct / 10);
+    const bar = '%'.repeat(loaded) + '.'.repeat(10 - loaded);
+
+    if (loaded === 10) {
+        console.log(`100% Complete!\n[${bar}]`);
+    } else {
+        console.log(`${pct}% [${bar}]\nStill loading...`);
+    }
 }
 
 loadingBar(30);
