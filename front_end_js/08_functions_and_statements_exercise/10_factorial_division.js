711c38eadd65a4abd516fe1820208a75292830b2
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-03-06 15:57:15 +0000

    10

--- front_end_js/08_functions_and_statements_exercise/10_factorial_division.js
+++ front_end_js/08_functions_and_statements_exercise/10_factorial_division.js
@@ -1,2 +1,17 @@
 //(async ()=>{let primes=n=>{let p=[2],i=3;while(p.length<n){if(p.every(j=>i%j))p.push(i);i++}return p}, fib=(n,a=0,b=1)=>n?fib(n-1,b,a+b):a, quote=await fetch('https://api.quotable.io/random').then(r=>r.json()).then(d=>d.content), title=await fetch('https://nova.bg/').then(r=>r.text()).then(t=>t.match(/<title>(.*?)<\/title>/)[1]); console.log(`🔢 First 10 Primes: ${primes(10)}\n🐍 10th Fibonacci: ${fib(10)}\n📝 Quote: "${quote}"\n🌍 Webpage Title: "${title}"`);})();
 
+function fac(n1, n2){
+    function factorial(n) {
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+
+    let fact1 = factorial(n1);
+    let fact2 = factorial(n2);
+    let divisionResult = fact1 / fact2;
+
+    console.log(divisionResult.toFixed(2));
+
+}
+
+fac(5, 2);
+fac(6, 2);
