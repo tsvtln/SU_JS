c7634aebfd8f350fd144ad43566725afcc6072fa
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-03-06 15:02:12 +0000

    06

--- front_end_js/08_functions_and_statements_exercise/06_password_validators.js
+++ front_end_js/08_functions_and_statements_exercise/06_password_validators.js
@@ -1,10 +1,10 @@
-function passwordValidator(password){
+function passwordValidator(pwd){
     let isValid = true;let tp = '';
     function checkTP(){if(tp.length>1){tp+='\n';}}
     function checkLength(p){if(p.length < 6 || p.length > 10){tp = 'Password must be between 6 and 10 characters';isValid = false;}}
     function checkSymbols(p){if(!(/^[a-zA-Z0-9]+$/.test(p))){checkTP();tp += 'Password must consist only of letters and digits';isValid = false;}}
     function checkDigits(p) {if (!((p.match(/\d/g) || []).length >= 2)){checkTP();tp += 'Password must have at least 2 digits';isValid = false;}}
-    checkLength(password);checkSymbols(password);checkDigits(password);
+    checkLength(pwd);checkSymbols(pwd);checkDigits(pwd);
     if(isValid){tp = 'Password is valid'}
     console.log(tp);
 }
