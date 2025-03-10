01c7fc846547fb140e574a9a7b56b63733ad7ef9
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2025-03-10 09:02:22 +0000

    02

---
+++ front_end_js/09_objects_and_classes/02_city.js
@@ -0,0 +1,8 @@
+function cityInfo(obj){
+    for (let [key, value] of Object.entries(obj)) {
+        console.log(`${key} -> ${value}`);
+    }
+}
+
+cityInfo({name: "Sofia",area: 492,population: 1238438,country: "Bulgaria",postCode: "1000"});
+cityInfo({name: "Plovdiv",area: 389,population: 1162358,country: "Bulgaria",postCode: "4000"});
