250c98e94d8b05ce7b76fdbbdc8329e5923ca7b1
tsvtln <tsvtln@abv.bg>
2025-03-20 16:24:44 +0200

    Update collect-list-items.js

--- front_end_js/11_dom_intruduction/02.Collect List Items/collect-list-items.js
+++ front_end_js/11_dom_intruduction/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,15 @@
 function extractText() {
-    // TODO
+    //alert('it works');
+
+    let list = document.getElementById('items');
+    let items = list.children;
+    let result = [];
+
+    for (let li of items) {
+        result.push(li.textContent);
+    }
+
+    let output = document.getElementById('result');
+    output.value = result.join('\n')
+
 }
