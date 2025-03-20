473e82e85a3932066e0b796b08f84c760c989f88
tsvtln <tsvtln@abv.bg>
2025-03-20 17:53:28 +0200

    Update colorize-table.js

--- front_end_js/11_dom_intruduction/05.Colorize Table/colorize-table.js
+++ front_end_js/11_dom_intruduction/05.Colorize Table/colorize-table.js
@@ -1,3 +1,7 @@
 function colorize() {
-    //TODO
+    let rows = document.querySelectorAll('tbody tr:nth-child(even)');
+
+    for (let row of rows) {
+        row.style.backgroundColor = 'teal';
+    }
 }
