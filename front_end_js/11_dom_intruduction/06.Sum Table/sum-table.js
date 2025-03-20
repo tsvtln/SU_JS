2e882d778e75c7b55a52836b43ccf4a88ad275c7
tsvtln <tsvtln@abv.bg>
2025-03-20 18:07:03 +0200

    Update sum-table.js

--- front_end_js/11_dom_intruduction/06.Sum Table/sum-table.js
+++ front_end_js/11_dom_intruduction/06.Sum Table/sum-table.js
@@ -1,3 +1,15 @@
 function sumTable() {
-    // TODO
+    let rows = [...document.querySelectorAll('tbody tr')];
+    let output = rows.pop();
+
+    let sum = 0;
+
+    for (let row of rows) {
+        let col = row.children[row.children.length - 1];
+        sum += Number(col.textContent);
+    }
+
+    let outputCol = output.children[output.children.length - 1];
+    outputCol.textContent = sum;
+
 }
