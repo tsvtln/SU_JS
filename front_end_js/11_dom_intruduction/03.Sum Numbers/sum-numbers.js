e151fe3b63e0127518072704a96d84370720374c
tsvtln <tsvtln@abv.bg>
2025-03-20 16:32:40 +0200

    Update sum-numbers.js

--- front_end_js/11_dom_intruduction/03.Sum Numbers/sum-numbers.js
+++ front_end_js/11_dom_intruduction/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,11 @@
 function calc() {
-    // TODO
+    num1 = document.getElementById('num1')
+    num2 = document.getElementById('num2')
+
+    let a = Number(num1.value);
+    let b = Number(num2.value);
+    let sum = a + b;
+
+    let output = document.getElementById('sum');
+    output.value = sum;
 }
