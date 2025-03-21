aa9a33297c21155fbf04c1d319dbb2865d467a06
tsvtln <tsvtln@abv.bg>
2025-03-21 15:33:32 +0200

    Update generate-report.js

--- front_end_js/12_dom_introduction/08. Generate Report/generate-report.js
+++ front_end_js/12_dom_introduction/08. Generate Report/generate-report.js
@@ -1,3 +1,32 @@
 function solve() {
-    //TODO
+    let selected = new Map();
+    let headers = document.querySelectorAll('thead [type="checkbox"]');
+    let result = [];
+
+    for (let i=0; i < headers.length;i++){
+        if(headers[i].checked){
+            selected.set(i, headers[i].name);
+        }
+    }
+
+    let rows = document.querySelectorAll('tbody tr');
+
+    for (let row of rows){
+        let record = {};
+
+        for (let i=0; i<row.children.length; i++){
+            if (selected.has(i)){
+                let prorName = selected.get(i);
+                let col = row.children[i]
+                record[prorName] = col.textContent;
+            }
+        }
+    
+        result.push(record);
+
+    }
+
+    let output = document.getElementById('output');
+    output.value = JSON.stringify(result);
+
 }
