1a37c70316ab43753794456d97480509ed496f41
tsvtln <tsvtln@abv.bg>
2025-03-21 10:46:40 +0200

    04

--- front_end_js/12_dom_introduction/04. Search in List/search-in-list.js
+++ front_end_js/12_dom_introduction/04. Search in List/search-in-list.js
@@ -1,3 +1,17 @@
 function solve() {
-   // TODO
+   const towns = Array.from(document.querySelectorAll('ul li'));
+   const searchText = document.getElementById('searchText').value.toLowerCase();
+   let matches = 0;
+
+   for (let town of towns) {
+       town.innerHTML = town.textContent;
+
+       if (searchText !== '' && town.textContent.toLowerCase().includes(searchText)) {
+           const regex = new RegExp(`(${searchText})`, 'gi');
+           town.innerHTML = town.textContent.replace(regex, `<span style="font-weight: bold">$1</span>`);
+           matches++;
+       }
+   }
+
+   document.getElementById('result').textContent = `${matches} matches found`;
 }
