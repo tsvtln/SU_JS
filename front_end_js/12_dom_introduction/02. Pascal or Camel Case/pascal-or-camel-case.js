16ee4c6613d20bad1e5a2be565dcdc96fc623cb3
tsvtln <tsvtln@abv.bg>
2025-03-21 10:01:15 +0200

    Update pascal-or-camel-case.js

--- front_end_js/12_dom_introduction/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ front_end_js/12_dom_introduction/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO
+  const text = document.getElementById('text').value;
+  const namingConvention = document.getElementById('naming-convention').value;
+  let output = '';
+  let textArr = []
+  let textOut = []
+  let outputField = document.getElementById('result');
+
+  if (namingConvention === 'Camel Case'){
+    textArr = text.split(' ');
+    for (i = 0; i < textArr.length; i++){
+      if (i===0){
+        textOut.push(textArr[i].toLowerCase());
+      } else {
+        let wordToLower = textArr[i].toLowerCase();
+        let capitalized = wordToLower[0].toUpperCase() + wordToLower.slice(1);
+        textOut.push(capitalized);
+      }
+    }
+    output = textOut.join('');
+  } else if (namingConvention === 'Pascal Case'){
+    textArr = text.split(' ');
+    for (let word of textArr){
+      let wordToLower = word.toLowerCase();
+      let capitalized = wordToLower[0].toUpperCase() + wordToLower.slice(1);
+      textOut.push(capitalized);
+    }
+    console.log(textOut)
+    output = textOut.join('');
+  } else {
+    output = 'Error!'
+  }
+  outputField.textContent = output;
 }
