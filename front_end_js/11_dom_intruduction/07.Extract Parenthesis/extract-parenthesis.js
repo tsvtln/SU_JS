d5c3bcb5359cdcc4c2c0c28e879cda2532f8d786
tsvtln <tsvtln@abv.bg>
2025-03-21 09:15:44 +0200

    Update extract-parenthesis.js

--- front_end_js/11_dom_intruduction/07.Extract Parenthesis/extract-parenthesis.js
+++ front_end_js/11_dom_intruduction/07.Extract Parenthesis/extract-parenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
-    // TODO
+    let pattern = /\((.+?)\)/g;
+    let element = document.getElementById(content);
+    let text = element.textContent;
+    let match = pattern.exec(text);
+    let result = [];
+
+    while (match) {
+        result.push(match[1]);
+        match = pattern.exec(text)
+    }
+
+    return result.join('; ')
 }
