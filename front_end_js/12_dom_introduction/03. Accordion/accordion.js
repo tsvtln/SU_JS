a0040ae3710d7f8f4c76d8d82bf8c918f5a9834c
tsvtln <tsvtln@abv.bg>
2025-03-21 10:21:53 +0200

    03

--- front_end_js/12_dom_introduction/03. Accordion/accordion.js
+++ front_end_js/12_dom_introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    //TODO
+    let buttonMore = document.getElementsByClassName('button')[0];
+    let extra = document.getElementById('extra');
+
+    if (buttonMore.textContent === 'More'){
+        buttonMore.textContent = 'Less';
+        extra.style.display = 'block'
+    } else {
+        extra.style.display = 'none'
+        buttonMore.textContent = 'More';
+    }
 }
