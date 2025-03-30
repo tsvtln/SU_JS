e71a44f1319cce8d9024c7e38cfbed9b28d34ebd
tsvtln <tsvtln@abv.bg>
2025-03-31 00:08:00 +0300

    Update mouse-gradient.js

--- front_end_js/13_dom_manipulation_and_events/04. Mouse-Gradient/mouse-gradient.js
+++ front_end_js/13_dom_manipulation_and_events/04. Mouse-Gradient/mouse-gradient.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    //TODO
+    let gradient = document.getElementById('gradient');
+
+    gradient.addEventListener('mousemove', (ev) =>{
+        let value = Math.floor(ev.offsetX / gradient.clientWidth * 100);
+
+        document.getElementById('result').textContent = value + '%';
+    });
 }
