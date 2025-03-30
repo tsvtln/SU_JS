0117c2091833756d6e84b40e3b08526496b9160c
tsvtln <tsvtln@abv.bg>
2025-03-30 23:43:07 +0300

    Update add-delete.js

--- front_end_js/13_dom_manipulation_and_events/03. Add-Delete/add-delete.js
+++ front_end_js/13_dom_manipulation_and_events/03. Add-Delete/add-delete.js
@@ -1,3 +1,28 @@
 function addItem() {
-    // TODO
+    let input = document.getElementById('newItemText');
+    let text = input.value;
+
+    if (!text) {
+        return;
+    }
+
+    let newLi = document.createElement('li');
+    newLi.textContent = text;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+    newLi.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', onDelete);
+
+
+    let list = document.getElementById('items');
+    list.appendChild(newLi);
+
+    input.value = '';
+
+    function onDelete(e) {
+        let item = e.target.parentElement;
+        item.remove();
+    }
 }
