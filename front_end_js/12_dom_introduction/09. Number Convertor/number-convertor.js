59b5202ecb68ea6ebada3fbf9bbc3b4429cc985b
tsvtln <tsvtln@abv.bg>
2025-03-30 20:23:19 +0300

    Update number-convertor.js

--- front_end_js/12_dom_introduction/09. Number Convertor/number-convertor.js
+++ front_end_js/12_dom_introduction/09. Number Convertor/number-convertor.js
@@ -1,3 +1,14 @@
 function solve() {
-    // TODO
+    const number = Number(document.getElementById('input').value);
+    const convertTo = document.getElementById('selectMenuTo').value;
+
+    let result = '';
+
+    if (convertTo === 'binary') {
+        result = number.toString(2);
+    } else if (convertTo === 'hexadecimal') {
+        result = number.toString(16).toUpperCase();
+    }
+
+    document.getElementById('result').value = result;
 }
