0eda6fe6fc0f2849c7c603b45972246fdff7922e
tsvtln <tsvtln@abv.bg>
2025-03-31 00:28:01 +0300

    Update hightlight-active.js

--- front_end_js/13_dom_manipulation_and_events/05. Highlight-Active/hightlight-active.js
+++ front_end_js/13_dom_manipulation_and_events/05. Highlight-Active/hightlight-active.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', focused);
 
 function focused() {
-    //TODO
+    let inputs = document.querySelectorAll('input');
+
+    for (let input of inputs) {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+    }
+    
+    function onFocus(ev) {
+        ev.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(ev) {
+        ev.target.parentElement.classList.remove('focused');
+    }
 }
