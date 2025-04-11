f7d7678d9b728cc7995c73c7a2e00bc4586d8cae
tsvtln <tsvtln@abv.bg>
2025-04-11 18:08:41 +0300

    02 and 03

--- front_end_js/17_HTTP_and_AJAX/03.Phonebook/app.js
+++ front_end_js/17_HTTP_and_AJAX/03.Phonebook/app.js
@@ -1,5 +1,73 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click', onLoad);
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
 }
 
 attachEvents();
+
+async function onLoad() {
+    let phones = await loadPhones();
+
+    let phonebook = document.getElementById('phonebook');
+
+    phonebook.replaceChildren();
+    for (let phone of phones){
+        let row = document.createElement('li');
+        row.textContent = `${phone.person}: ${phone.phone}`;
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', () => onDelete(row, phone._id));
+
+        row.appendChild(deleteBtn);
+        phonebook.appendChild(row);
+    }
+}
+
+async function onDelete(row, phoneId) {
+    await deletePhoneById(phoneId);
+    row.remove();
+}
+
+async function onCreate() {
+    let personInput = document.getElementById('person');
+    let phoneInput = document.getElementById('phone');
+    
+    let person = personInput.value;
+    let phone = phoneInput.value;
+
+    if (!person || !phone) {
+        return;
+    }
+
+    await createPhones(person, phone);
+
+    onLoad();
+}
+
+async function loadPhones() {
+    let res = await fetch('http://localhost:3030/jsonstore/phonebook');
+    let data = await res.json();
+
+    return Object.values(data);
+}
+
+
+async function createPhones(person, phone) {
+    let record = {
+        person,
+        phone
+    }
+
+    let options = {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(record)
+    }
+
+    await fetch('http://localhost:3030/jsonstore/phonebook', options);
+}
+
+async function deletePhoneById(phoneId) {
+    await fetch('http://localhost:3030/jsonstore/phonebook/' + phoneId, {method: 'delete'});
+}
